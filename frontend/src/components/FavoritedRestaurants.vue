481741058135caabd263c9d6bebafff5d37a7377
Blake Wendelken <wendelkenb97@yahoo.com>
2020-08-10 13:15:01 -0400

    Pushing before review

---
+++ frontend/src/components/FavoritedRestaurants.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class = "restaurants">
+        <div>
+            <h1>Favorite Restaurants!</h1>
+          {{favoriteRestaurants.restaurantId}}
+        </div>
+    </div>    
+</template>
+
+
+<script>
+
+import favoriteRestaurantsService from '@/services/FavoriteRestaurantsService'
+
+export default {
+    name: 'favorited-restaurants',
+    data() {
+        return {
+          favoriteRestaurants: {
+              restaurantId: '',
+          }
+            }            
+        },
+    created() {
+        
+        favoriteRestaurantsService.getFavoriteRestaurants()
+        .then(response => {
+            this.favoriteRestaurants = response.data;
+                });
+            }
+    }
+        </script>
