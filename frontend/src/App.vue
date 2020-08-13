bbdff85b583dae3679c02387d8a824ebcca5c5c6
Jon Newell <loxmancv67@gmail.com>
2020-08-13 12:14:22 -0400

    moved navigation from App.vue to NavigationBar component and imported into each applicable view.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,19 @@
 <template>
-<body>
-  <div id="app">
-    <div id="nav" v-if="$store.state.token != '' && $store.state.isNewUser == false">
-      <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''">Home</router-link>&nbsp;| &nbsp;
-      <router-link v-bind:to="{ name: 'UpdateProfile' }" v-if="$store.state.token != ''">Questionnaire</router-link>&nbsp;| &nbsp;
-      <router-link v-bind:to="{ name: 'favorites' }" v-if="$store.state.token != ''">Favorite Restaurants</router-link>&nbsp;| &nbsp;
-      <router-link v-bind:to="{ name: 'swipe' }" v-if="$store.state.token != ''">Food Finder</router-link>&nbsp;| &nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <router-view class = "body"/>
-  </div>
+  <body>
+    <router-view class = "body" />
   </body>
 </template>
 
 <style scoped>
-*{
-    font-family: "Impact", Charcoal, sans-serif;
-    color: white;
-    text-align: center;
-    background-image:url('./assets/backgroundpicture.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-}
+
 
 .body{
+  font-family: "Impact", Charcoal, sans-serif;
+  color: rgb(97, 26, 26);
+  text-align: center;
+  background-image:url('./assets/backgroundpicture.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
   height: 100vh;
-}
-
-#nav{
-  display: flex;
-  justify-content: center;
 }
 </style>
