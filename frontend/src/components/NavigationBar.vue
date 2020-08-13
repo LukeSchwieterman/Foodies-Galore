269e122b957524a28a7f3690df9bf77987f59085
Jon Newell <loxmancv67@gmail.com>
2020-08-13 12:22:21 -0400

    added color to the nav bar links.

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>  
     <div id="nav" v-if="$store.state.token != '' && $store.state.isNewUser == false">
-      <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''">Home</router-link>&nbsp;| &nbsp;
+      <router-link class="color black" v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''">Home</router-link>&nbsp;| &nbsp;
       <router-link v-bind:to="{ name: 'UpdateProfile' }" v-if="$store.state.token != ''">Questionnaire</router-link>&nbsp;| &nbsp;
       <router-link v-bind:to="{ name: 'favorites' }" v-if="$store.state.token != ''">Favorite Restaurants</router-link>&nbsp;| &nbsp;
       <router-link v-bind:to="{ name: 'swipe' }" v-if="$store.state.token != ''">Food Finder</router-link>&nbsp;| &nbsp;
@@ -21,11 +21,15 @@ export default {
   display: flex;
   justify-content: space-around;
   padding: 0.3rem 1rem;
-  color:black;
+  color:black; /* color of dividers */
   background-color: rgba(238, 113, 11);
   border: 1px solid rgba(238, 113, 11);
   border-radius: 9px;
   margin:auto;
   width: 35%
+}
+
+a {
+    color: black; /* color of link  */
 }
 </style>
